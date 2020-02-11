3ba857f56d6b65febca6e87b11d79b1201cf538f
Fadilah Hasan <fadilah.hasan@metraplasa.co.id>
2020-02-11 15:55:00 +0700

    first init

---
+++ Xcode Template Manager/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Xcode Template Manager
+//
+//  Created by Fadilah Hasan on 11/02/20.
+//  Copyright © 2020 Fadilah Hasan. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Xcode Template Manager/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Xcode Template Manager
+//
+//  Created by Fadilah Hasan on 11/02/20.
+//  Copyright © 2020 Fadilah Hasan. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
